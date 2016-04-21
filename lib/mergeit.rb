a6d4487bd3e2df60e8736afd74fa7de5862c13ef
daneb <dane.balia@hetzner.co.za>
2016-04-21 23:22:20 +0200

    Working initialization and validation

--- lib/mergeit.rb
+++ lib/mergeit.rb
@@ -2,13 +2,52 @@ require "mergeit/version"
 
 module Mergeit
   class Merge
-    def initialize
+    attr_reader :input_files, :errors
+
+    def initialize(input_files)
+      @errors = []
+      @input_files = input_files
+      print_errors_and_exit unless validate_input_files
     end
 
-    def say_hello(files_array)
-      puts 'Hello World'
-      puts files_array[0]
-      puts files_array[1]
+    protected
+    def print_errors_and_exit
+      @errors.each { |error| puts error }
+      exit
+    end
+
+    def validate_input_files
+      validate_files_exist && validate_files_ascii
+    end
+
+    private
+    def validate_files_exist
+      exist_errors = check_if_files_exist
+      exist_errors.each {|error| @errors << error } unless exist_errors.empty?
+      @errors.empty?
+    end
+
+    def validate_files_ascii
+      ascii_errors = check_if_files_ascii
+      ascii_errors.each {|error| @errors << error } unless ascii_errors.empty?
+      @errors.empty?
+    end
+
+    def check_if_files_exist
+      result = []
+      @input_files.each { |filename|
+        result << "#{filename} does not exist" unless File.exist?(filename)
+      }
+      result
+    end
+
+    def check_if_files_ascii
+      result = []
+      @input_files.each { |filename|
+        file_type = %x[ file #{filename} ]
+        result << "#{filename} not ascii" unless file_type =~ /ascii/i
+      }
+      result
     end
   end
 end
--- spec/merge_spec.rb
+++ spec/merge_spec.rb
@@ -2,7 +2,35 @@ require 'spec_helper'
 
  describe Mergeit::Merge do
   before :each do
+    @file1 = 'spec/support/file1.txt'
+    @file2 = 'spec/support/file2.txt'
+    @iut = Mergeit::Merge.new([@file1, @file2])
 
+    # Send Protected Methods to Public
+    Mergeit::Merge.send(:public, *Mergeit::Merge.protected_instance_methods)
+  end
+
+  context 'Initialization' do
+    it 'should receive an array with two files' do
+      expect(@iut.input_files == [@file1, @file2]).to be true
+    end
+
+    it 'should validate that the input is an array' do
+      expect(@iut.input_files.class == Array).to eq true 
+    end
+
+    it 'should check the files exists' do
+      expect(@iut.validate_input_files).to eq true
+    end
+
+    it 'should notify if any or both files do not exist' do
+      dud_file_iut = Mergeit::Merge.new([@file1, '/etc/hostages'])
+      expect(dud_file_iut.validate_input_files).to eq ["/etc/hostages does not exist"]
+    end
+
+    it 'should confirm if the files are ASCII' do
+      expect(@iut.validate_input_files).to be true
+    end
   end
 
   context 'Open files individually' do
