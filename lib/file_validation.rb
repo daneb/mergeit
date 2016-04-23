2514ac7e1f82dfcc668a003e930bc123574c6092
daneb <dane.balia@hetzner.co.za>
2016-04-23 03:08:24 +0200

    Added README info

--- lib/file_validation.rb
+++ lib/file_validation.rb
@@ -1,37 +1,44 @@
-class FileValidation
-  attr_accessor :errors
+module Mergeit
+  # Used to Validate The Input Files
+  class FileValidation
+    attr_accessor :errors
 
-  def initialize(input_files)
-    @errors = []
-    @input_files = input_files
-  end
+    def initialize(input_files)
+      @errors = []
+      @input_files = input_files
+    end
 
-  def files_exist?
-    exist_errors = check_if_files_exist
-    exist_errors.each {|error| @errors << error } unless exist_errors.empty?
-    @errors.empty?
-  end
+    # Checks if the files actually exist at the specified location
+    def files_exist?
+      exist_errors = check_if_files_exist
+      exist_errors.each {|error| @errors << error } unless exist_errors.empty?
+      @errors.empty?
+    end
 
-  def files_are_ascii?
-    ascii_errors = check_if_files_ascii
-    ascii_errors.each {|error| @errors << error } unless ascii_errors.empty?
-    @errors.empty?
-  end
+    # Checks if the files are actual text files
+    def files_are_ascii?
+      ascii_errors = check_if_files_ascii
+      ascii_errors.each {|error| @errors << error } unless ascii_errors.empty?
+      @errors.empty?
+    end
 
-  def check_if_files_exist
-    result = []
-    @input_files.each { |filename|
-      result << "#{filename} does not exist" unless File.exist?(filename)
-    }
-    result
-  end
+    # Supporting method that actual does the word for a files existence
+    def check_if_files_exist
+      result = []
+      @input_files.each { |filename|
+        result << "#{filename} does not exist" unless File.exist?(filename)
+      }
+      result
+    end
 
-  def check_if_files_ascii
-    result = []
-    @input_files.each { |filename|
-      file_type = %x[ file #{filename} ]
-      result << "#{filename} not ascii" unless file_type =~ /ascii/i
-    }
-    result
+    # Supporting method that checks if a file is ascii
+    def check_if_files_ascii
+      result = []
+      @input_files.each { |filename|
+        file_type = %x[ file #{filename} ]
+        result << "#{filename} not ascii" unless file_type =~ /ascii/i
+      }
+      result
+    end
   end
 end
--- lib/mergeit/config.rb
+++ lib/mergeit/config.rb
@@ -1,8 +1,14 @@
 module Mergeit
+  # Using Ruby For Configuration
   module Config
+    # Delimiter for how a line is divided up into parts
     FILE_FORMAT = { delimeter: ':'}
+    # Determines after splitting on delimeter how many parts there should be
     SUPPORTED_PARTS = { size: 2 }
+    # Regex that verfies a CSV (supports floating-point and integers) 
+    # @note Does not support e notation
     CSV_FORMAT = { regex: '(^((\d+,)|(\d+[,.]\d+))+(\d+|(\d+[,.]\d+))$)' }
+    # Delimiter for splitting the second part (aka. the numbers)
     NUMBERS = { delimeter: ','}
   end
 end
